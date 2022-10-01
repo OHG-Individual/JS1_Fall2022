0e4f01dbfaa5442a1ab1832c1b33ca666aac99b8
Oscar Hernandez-Gomez <ohernandezgomez@gmail.com>
2022-10-01 12:05:34 -0700

    adds map UI

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import custom from "./custom-style.json";
 let map;
 
 async function init() {
+    const neighborhoods = await import("../data/output.json");
     const style = map.getStyle();
 
     style.sources = {
@@ -15,6 +16,67 @@ async function init() {
     };
     style.layers.push(...custom.layers);
     map.setStyle(style);
+
+    map.getSource("neighborhoods").setData(neighborhoods);
+
+    initPopup();
+    initLegend();
+}
+
+function initLegend() {
+    const legend = document.querySelector('#legend');
+    const template = document.querySelector('#legend-entry');
+    const fillColorStyle = map.getPaintProperty('neighborhoods-fill', 'fill-extrusion-color');
+
+    fillColorStyle.splice(0,2);
+    let total = 0;
+
+    for (let index = 0; index < fillColorStyle.length; index+=2) {
+        const entry = document.importNode(template.Content, true);
+        const spans = entry.querySelectorAll("span");
+        const color = fillColorStyle[index];
+        const siteCount = fillColorStyle[index+1]
+
+        spans[0].style.backgroundColor = color;
+
+        if(index === fillColorStyle.length-1) {
+            spans[1].textcontent = ">=" + total;
+        } else{
+            spans[1].textContent = total + "-" +(siteCount - 1);
+            total = siteCount;
+        }
+
+        legend.appendChild(entry)
+    }
+}
+
+let hovered;
+const popup = document.querySelector('popup')
+const neighborhoodName = popup.querySelector('.neighborhood-name');
+const count = popup.querySelector('.count');
+function initPopup() {
+    map.on('mousemove', 'neighborhoods-fill', function(event) {
+        clearHover();
+        if(event.features.length > 0) {
+            hovered = event.features[0];
+            map.setFeatureState(hovered, {
+                hover: true
+            });
+            popup.style.display = "block";
+            neighborhoodName.textContent = hovered.properties.name;
+            count.textContent = hovered.properties.count;
+        }
+    });
+    map.on('mouseleave', 'neighborhoods-fill',clearHover);
+}
+
+function clearHover() {
+    if (hovered) {
+        map.setFeatureState(hovered, {
+            hover: false
+        });
+    }
+    popup.style.display = "none";
 }
 
 mapboxgl.accessToken = settings.accessToken;
